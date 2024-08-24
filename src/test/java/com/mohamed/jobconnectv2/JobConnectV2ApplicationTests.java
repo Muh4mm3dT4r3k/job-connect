d6dcba4d403a32a96616c9feb69efa5d9035696c
Mohamed Tarek <mohamedtarek3.2001@gmail.com>
2024-08-25 01:24:54 +0300

    init commit

---
+++ src/test/java/com/mohamed/jobconnectv2/JobConnectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.jobconnectv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobConnectV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
