f40f9c94d7cd624171a5b0d22e05697b6ac13bf2
Mohamed Tarek <mohamedtarek3.2001@gmail.com>
2024-08-25 14:21:48 +0300

    implement AuthenticationController

--- src/main/java/com/mohamed/jobconnectv2/auth/AuthenticationController.java
+++ src/main/java/com/mohamed/jobconnectv2/auth/AuthenticationController.java
@@ -1,9 +1,30 @@
 package com.mohamed.jobconnectv2.auth;
 
+import com.mohamed.jobconnectv2.auth.dto.UserLoginRequest;
+import com.mohamed.jobconnectv2.auth.dto.UserLoginResponse;
+import com.mohamed.jobconnectv2.auth.dto.UserRegisterRequest;
+import com.mohamed.jobconnectv2.auth.dto.UserRegisterResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+
+
+    @PostMapping("/register")
+    public UserRegisterResponse register(UserRegisterRequest request){
+        return authenticationService.register(request);
+    }
+
+
+    @PostMapping("/login")
+    public UserLoginResponse loginResponse(UserLoginRequest request){
+        return authenticationService.login(request);
+    }
 }
