20bcf7211f9ef37d216f8602a8bdf47a3ba23122
Mohamed Tarek <mohamedtarek3.2001@gmail.com>
2024-08-28 00:30:06 +0300

    add @RequestBody in auth controller

--- src/main/java/com/mohamed/jobconnectv2/auth/AuthenticationController.java
+++ src/main/java/com/mohamed/jobconnectv2/auth/AuthenticationController.java
@@ -4,8 +4,10 @@ import com.mohamed.jobconnectv2.auth.dto.UserLoginRequest;
 import com.mohamed.jobconnectv2.auth.dto.UserLoginResponse;
 import com.mohamed.jobconnectv2.auth.dto.UserRegisterRequest;
 import com.mohamed.jobconnectv2.auth.dto.UserRegisterResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,13 +20,13 @@ public class AuthenticationController {
 
 
     @PostMapping("/register")
-    public UserRegisterResponse register(UserRegisterRequest request){
+    public UserRegisterResponse register(@RequestBody @Valid UserRegisterRequest request){
         return authenticationService.register(request);
     }
 
 
     @PostMapping("/login")
-    public UserLoginResponse loginResponse(UserLoginRequest request){
+    public UserLoginResponse loginResponse(@RequestBody @Valid UserLoginRequest request){
         return authenticationService.login(request);
     }
 }
