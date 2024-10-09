e1c21517bb30f2d51a4126039c0098b4c13e3e8d
Mohamed Tarek <mohamedtarek3.2001@gmail.com>
2024-10-09 12:53:36 +0300

    Refactor CommonMethods to use static methods with repository parameters
    - Removed `private final UserRepository userRepository` and `private final RoleRepository roleRepository`
    - Updated methods to accept `UserRepository` and `RoleRepository` as parameters
    - Converted methods to static for utility-style usage

--- src/main/java/com/mohamed/jobconnectv2/common/CommonMethods.java
+++ src/main/java/com/mohamed/jobconnectv2/common/CommonMethods.java
@@ -4,40 +4,34 @@ import com.mohamed.jobconnectv2.role.Role;
 import com.mohamed.jobconnectv2.role.RoleRepository;
 import com.mohamed.jobconnectv2.user.User;
 import com.mohamed.jobconnectv2.user.UserRepository;
-import jakarta.validation.constraints.NotNull;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Component
-@RequiredArgsConstructor
+
 public class CommonMethods {
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
     
-    public  User findUserByIdOrThrow(@NotNull UUID userId) {
+    public static User findUserByIdOrThrow(UUID userId, UserRepository userRepository) {
         return userRepository
                 .findById(userId)
                 .orElseThrow(() -> new RuntimeException("user not found"));
         // TODO handle exception
     }
 
-    public  Role findRoleByName(String role) {
+    public static Role findRoleByName(String role, RoleRepository roleRepository) {
         return roleRepository
                 .findByName(role)
                 .orElseThrow(() -> new RuntimeException("Role not found"));
         // TODO handle exception
     }
 
-    public  void checkUsernameAlreadyExist(String username) {
+    public static void checkUsernameAlreadyExist(String username, UserRepository userRepository) {
         boolean existsUserByUsername = userRepository.existsUserByUsername(username);
         if (existsUserByUsername)
             throw new RuntimeException("username already exists");
         //TODO handle exception
     }
 
-    public  void checkEmailAlreadyExist(String email) {
+    public static void checkEmailAlreadyExist(String email, UserRepository userRepository) {
         boolean existsUserByEmail = userRepository.existsUserByEmail(email);
         if (existsUserByEmail)
             throw new RuntimeException("email already exists");
