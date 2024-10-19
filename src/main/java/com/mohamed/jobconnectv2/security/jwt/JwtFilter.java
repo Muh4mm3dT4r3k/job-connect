074d6f9a250f3406a56aef8ad3759ad64ae05ab6
Mohamed Tarek <mohamedtarek3.2001@gmail.com>
2024-10-19 13:51:18 +0300

    set userDetails object instead of WebAuthenticationDetails object

--- src/main/java/com/mohamed/jobconnectv2/security/jwt/JwtFilter.java
+++ src/main/java/com/mohamed/jobconnectv2/security/jwt/JwtFilter.java
@@ -1,5 +1,6 @@
 package com.mohamed.jobconnectv2.security.jwt;
 
+import com.mohamed.jobconnectv2.user.User;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,7 +11,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -28,7 +28,7 @@ public class JwtFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        if (request.getContextPath().contains("/api/v1/auth")) {
+        if (request.getRequestURI().contains("/api/v1/auth") || request.getRequestURI().contains("/api/v1/swagger-ui")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -51,11 +51,9 @@ public class JwtFilter extends OncePerRequestFilter {
                         userDetails.getAuthorities()
                 );
 
-                authenticationToken
-                        .setDetails(new WebAuthenticationDetailsSource()
-                        .buildDetails(request));
-                SecurityContextHolder.
-                        getContext()
+                authenticationToken.setDetails(userDetails);
+                SecurityContextHolder
+                        .getContext()
                         .setAuthentication(authenticationToken);
             }
         }
