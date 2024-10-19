8c73d4c1c21f8ba6b9d438e246f06b0cd979ad0e
Mohamed Tarek <mohamedtarek3.2001@gmail.com>
2024-10-19 13:55:56 +0300

    add role to db

--- src/main/java/com/mohamed/jobconnectv2/JobConnectV2Application.java
+++ src/main/java/com/mohamed/jobconnectv2/JobConnectV2Application.java
@@ -1,13 +1,31 @@
 package com.mohamed.jobconnectv2;
 
+import com.mohamed.jobconnectv2.role.Role;
+import com.mohamed.jobconnectv2.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @SpringBootApplication
 public class JobConnectV2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(JobConnectV2Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+        return args -> {
+            Role role = new Role();
+            role.setId(UUID.randomUUID());
+            role.setName("JOB_SEEKER");
+            role.setCreatedAt(LocalDateTime.now());
+            roleRepository.save(role);
+        };
     }
 
 }
