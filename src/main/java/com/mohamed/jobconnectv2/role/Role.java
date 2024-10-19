da26fb67d66e45690d0d7d6bb8e328bc6076bc44
Mohamed Tarek <mohamedtarek3.2001@gmail.com>
2024-10-19 13:45:02 +0300

    add getters and setters to Role

--- src/main/java/com/mohamed/jobconnectv2/role/Role.java
+++ src/main/java/com/mohamed/jobconnectv2/role/Role.java
@@ -3,6 +3,8 @@ package com.mohamed.jobconnectv2.role;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.time.LocalDateTime;
@@ -10,6 +12,8 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "_role")
+@Getter
+@Setter
 public class Role implements GrantedAuthority {
     @Id
     private UUID id;
